831baae70dd1ba7ce506fdaddc11332f7413c55b
KarlDarlin <nzatsikondji404@gmail.com>
2024-10-21 22:27:36 +0200

    amelioration de la blockchain

--- BlockchainC++/Blockchain/Blockchain.cpp
+++ BlockchainC++/Blockchain/Blockchain.cpp
@@ -1,69 +1,70 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <sstream>
 #include <string>
+#include <sstream>
 
-class Block {
+class Bloc
+{
 public:
-    int index;
-    std::string previous_hash;
-    long timestamp;
-    std::string data;
-    std::string hash;
+    int numero;
+    std::string hachagePrecedent;
+    long horodatage;
+    std::string donnees;
+    std::string hachageActuel;
 
-    Block(int idx, std::string prev_hash, long time, std::string d, std::string h)
-        : index(idx), previous_hash(prev_hash), timestamp(time), data(d), hash(h) {}
+    Bloc(int numeroBloc, std::string hachPrecedent, long temps, std::string data, std::string hachage)
+        : numero(numeroBloc), hachagePrecedent(hachPrecedent), horodatage(temps), donnees(data), hachageActuel(hachage) {}
 };
 
-// Fonction pour calculer un hash simplifié
-std::string calculate_hash(int index, std::string previous_hash, long timestamp, std::string data) {
-    std::stringstream ss;
-    ss << index << previous_hash << timestamp << data;
-    return std::to_string(std::hash<std::string>{}(ss.str()));
+std::string genererHachage(int numero, std::string hachagePrecedent, long horodatage, std::string donnees)
+{
+    std::ostringstream flux;
+    flux << numero << hachagePrecedent << horodatage << donnees;
+    return std::to_string(std::hash<std::string>{}(flux.str()));
 }
 
-// Fonction pour créer le bloc genesis
-Block create_genesis_block() {
-    long timestamp = std::time(nullptr);
-    std::string genesis_hash = calculate_hash(0, "0", timestamp, "Genesis Block");
-    return Block(0, "0", timestamp, "Genesis Block", genesis_hash);
+Bloc creerBlocGenesis()
+{
+    long horodatageInitial = std::time(nullptr);
+    std::string hachageGenesis = genererHachage(0, "0", horodatageInitial, "BlocGenesis");
+    return Bloc(0, "0", horodatageInitial, "BlocGenesis", hachageGenesis);
 }
 
-// Fonction pour créer un nouveau bloc
-Block create_new_block(Block previous_block, std::string data) {
-    int index = previous_block.index + 1;
-    long timestamp = std::time(nullptr);
-    std::string previous_hash = previous_block.hash;
-    std::string hash = calculate_hash(index, previous_hash, timestamp, data);
-    return Block(index, previous_hash, timestamp, data, hash);
+Bloc creerNouveauBloc(const Bloc& blocPrecedent, const std::string& donnees)
+{
+    int nouveauNumero = blocPrecedent.numero + 1;
+    long nouveauHorodatage = std::time(nullptr);
+    std::string hachagePrecedent = blocPrecedent.hachageActuel;
+    std::string hachageNouveau = genererHachage(nouveauNumero, hachagePrecedent, nouveauHorodatage, donnees);
+    return Bloc(nouveauNumero, hachagePrecedent, nouveauHorodatage, donnees, hachageNouveau);
 }
 
-int main() {
-    // Créer la blockchain et ajouter le bloc Genesis
-    std::vector<Block> blockchain;
-    Block genesis_block = create_genesis_block();
-    blockchain.push_back(genesis_block);
+int main()
+{
+    std::vector<Bloc> Blockchain;
 
-    // les détails du bloc Genesis
-    std::cout << "Le Block #" << genesis_block.index << " (Genesis) a ete ajoute a la blockchain" << std::endl;
-    std::cout << "Hash: " << genesis_block.hash << std::endl;
-    std::cout << "Previous Hash: " << genesis_block.previous_hash << std::endl << std::endl;
+    Bloc blocGenesis = creerBlocGenesis();
+    Blockchain.push_back(blocGenesis);
 
-    // Pour Ajouter quelques blocs à la blockchain
-    int num_of_blocks_to_add = 5;
-    Block previous_block = blockchain.back();
+    std::cout << "Le bloc #" << blocGenesis.numero << " (Genesis) est la base de la blockchain" << std::endl;
+    std::cout << "Hachage : " << blocGenesis.hachageActuel << std::endl;
+    std::cout << "Hachage precedent : " << blocGenesis.hachagePrecedent << std::endl << std::endl;
 
-    for (int i = 0; i < num_of_blocks_to_add; ++i) {
-        std::string new_block_data = "Block " + std::to_string(i + 1) + " Data";
-        Block new_block = create_new_block(previous_block, new_block_data);
-        blockchain.push_back(new_block);
+    Bloc dernierBloc = Blockchain.back();
+    const int totalBlocs = 5;
 
-        std::cout << "Le Block #" << new_block.index << " a ete ajoute a la blockchain" << std::endl;
-        std::cout << "Hash: " << new_block.hash << std::endl;
-        std::cout << "Previous Hash: " << previous_block.hash << std::endl << std::endl;
+    for (int i = 0; i < totalBlocs; ++i)
+    {
+        std::string contenuBloc = "Bloc " + std::to_string(i + 1) + " Donnees";
+        Bloc nouveauBloc = creerNouveauBloc(dernierBloc, contenuBloc);
+        Blockchain.push_back(nouveauBloc);
 
-        previous_block = new_block;
+        std::cout << "Le bloc #" << nouveauBloc.numero << " a ete ajoute a la blockchain" << std::endl;
+        std::cout << "Hachage : " << nouveauBloc.hachageActuel << std::endl;
+        std::cout << "Hachage precedent : " << dernierBloc.hachageActuel << std::endl << std::endl;
+
+        dernierBloc = nouveauBloc;
     }
 
     return 0;
