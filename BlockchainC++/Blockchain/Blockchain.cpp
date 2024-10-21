6f6da139dca045961b1d21425ffee09d4e7318db
KarlDarlin <nzatsikondji404@gmail.com>
2024-10-21 23:50:56 +0200

    ajout d'un menu pour gerer la blockchain

--- BlockchainC++/Blockchain/Blockchain.cpp
+++ BlockchainC++/Blockchain/Blockchain.cpp
@@ -40,31 +40,92 @@ Bloc creerNouveauBloc(const Bloc& blocPrecedent, const std::string& donnees)
     return Bloc(nouveauNumero, hachagePrecedent, nouveauHorodatage, donnees, hachageNouveau);
 }
 
+void afficherBlockchain(const std::vector<Bloc>& blockchain)
+{
+    std::cout << "\n===== Blockchain actuelle =====" << std::endl;
+    for (const auto& bloc : blockchain)
+    {
+        std::cout << "Bloc #" << bloc.numero << std::endl;
+        std::cout << "Hachage : " << bloc.hachageActuel << std::endl;
+        std::cout << "Hachage precedent : " << bloc.hachagePrecedent << std::endl;
+        std::cout << "Donnees : " << bloc.donnees << std::endl;
+        std::cout << "-----------------------------" << std::endl;
+    }
+}
+
+void supprimerDernierBloc(std::vector<Bloc>& blockchain)
+{
+    if (blockchain.size() > 1)
+    {
+        blockchain.pop_back();
+        std::cout << "Le dernier bloc a ete supprime." << std::endl;
+    }
+    else
+    {
+        std::cout << "non pelo on se supprime pas ca." << std::endl;
+    }
+}
+
+void ajouterBloc(std::vector<Bloc>& blockchain)
+{
+    Bloc dernierBloc = blockchain.back();
+    std::string contenuBloc;
+    std::cout << "Entrez les donnees pour le nouveau bloc : ";
+    std::cin.ignore();  
+    std::getline(std::cin, contenuBloc);
+
+    Bloc nouveauBloc = creerNouveauBloc(dernierBloc, contenuBloc);
+    blockchain.push_back(nouveauBloc);
+
+    std::cout << "Le bloc #" << nouveauBloc.numero << " a ete ajoute a la blockchain." << std::endl;
+    std::cout << "Hachage : " << nouveauBloc.hachageActuel << std::endl;
+    std::cout << "Hachage precedent : " << nouveauBloc.hachagePrecedent << std::endl;
+}
+
+void afficherMenu()
+{
+    std::cout << "\n===== Menu Blockchain =====" << std::endl;
+    std::cout << "1. Ajouter un nouveau bloc" << std::endl;
+    std::cout << "2. Supprimer le dernier bloc" << std::endl;
+    std::cout << "3. Quitter" << std::endl;
+    std::cout << "Fait toi plais pelo : ";
+}
+
 int main()
 {
     std::vector<Bloc> Blockchain;
 
     Bloc blocGenesis = creerBlocGenesis();
     Blockchain.push_back(blocGenesis);
+    std::string donneesPremierBloc = "Premier Bloc Donnees";
+    Bloc premierBloc = creerNouveauBloc(blocGenesis, donneesPremierBloc);
+    Blockchain.push_back(premierBloc);
+    afficherBlockchain(Blockchain);
 
-    std::cout << "Le bloc #" << blocGenesis.numero << " (Genesis) est la base de la blockchain" << std::endl;
-    std::cout << "Hachage : " << blocGenesis.hachageActuel << std::endl;
-    std::cout << "Hachage precedent : " << blocGenesis.hachagePrecedent << std::endl << std::endl;
-
-    Bloc dernierBloc = Blockchain.back();
-    const int totalBlocs = 5;
-
-    for (int i = 0; i < totalBlocs; ++i)
+    int choix;
+    bool continuer = true;
+    while (continuer)
     {
-        std::string contenuBloc = "Bloc " + std::to_string(i + 1) + " Donnees";
-        Bloc nouveauBloc = creerNouveauBloc(dernierBloc, contenuBloc);
-        Blockchain.push_back(nouveauBloc);
+        afficherMenu();
+        std::cin >> choix;
 
-        std::cout << "Le bloc #" << nouveauBloc.numero << " a ete ajoute a la blockchain" << std::endl;
-        std::cout << "Hachage : " << nouveauBloc.hachageActuel << std::endl;
-        std::cout << "Hachage precedent : " << dernierBloc.hachageActuel << std::endl << std::endl;
-
-        dernierBloc = nouveauBloc;
+        switch (choix)
+        {
+        case 1:
+            ajouterBloc(Blockchain);
+            afficherBlockchain(Blockchain); 
+            break;
+        case 2:
+            supprimerDernierBloc(Blockchain);
+            afficherBlockchain(Blockchain);  
+            break;
+        case 3:
+            continuer = false;
+            std::cout << "Au revoir!" << std::endl;
+            break;
+        default:
+            std::cout << "Option invalide. Veuillez reessayer." << std::endl;
+        }
     }
 
     return 0;
